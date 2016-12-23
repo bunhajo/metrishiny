b1956db265a19d3257bd95c24735ed039e855253
Juan Tornero Lucas <Juan Tornero Lucas>
2016-12-23 13:08:28 +0100

    Añadir Columnas, more options y reactive plot

--- ui.R
+++ ui.R
@@ -1,27 +1,29 @@
 library(metrigraphics)
 
 shinyUI(pageWithSidebar(
-  headerPanel("CSV File Upload Demo"),
-  
+  headerPanel("Metrigraphics"),
   sidebarPanel(
     #Selector for file upload
     fileInput('datafile', 'Choose CSV file',
-              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+              accept=c('text/csv', 'text/comma-separated-values','text/plain', ".csv", ".txt")),
     #These column selectors are dynamically created when the file is loaded
     uiOutput("fromCol"),
-    uiOutput("toCol")
-    #The conditional panel is triggered by the preceding checkbox
-    # conditionalPanel(
-    #   condition="input.amountflag==true",
-    #   uiOutput("amountCol")
-    # )
-    #The action button prevents an action firing before we're ready
-    #actionButton("getgeo", "Get geodata")
+    uiOutput("toCol"),
+    uiOutput("Title"),
+    uiOutput("moreoptions"),
+    conditionalPanel(
+      condition="input.moreoptions==true",
+      uiOutput("min")
+    ),  
+  
+    actionButton("plotear", "Plot")
     
   ),
   mainPanel(
-    headerPanel("Plot"),
+    #img(src = "C:/Users/juan_/Downloads/Scripts/Shiny/project metrigraphics/metrishiny/www/logo.png", height = 50, width = 100),
+    headerPanel("Archivo"),
     tableOutput("filetable"),
+    headerPanel("Plot"),
     plotOutput("newplot", width = "720px", height = "720px")
   )
 ))
